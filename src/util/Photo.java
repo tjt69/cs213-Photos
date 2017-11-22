fe2db255a53ed64892201a82cbd08ae5a86b7025
tjt69 <tjt69@rutgers.edu>
2017-11-22 17:47:03 -0500

    h

--- src/util/Photo.java
+++ src/util/Photo.java
@@ -43,7 +43,7 @@ public class Photo implements Serializable{
 	
 	public boolean containsTag(Tag t) {
 		for(Tag i : this.tags) {
-			if(t.equals(i)) {
+			if(i.equals(t)) {
 				return true;
 			}
 		}
